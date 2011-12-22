8c62750b486eb1d8ab91910d6f2025c4333ed21e
Jasson Casey <jasson.casey@gmail.com>
2011-12-22 11:12:37 -0600

    Initial simulation code.

---
+++ Simulation.h
@@ -0,0 +1,24 @@
+#ifndef SIMULATION_H
+#define SIMULATION_H
+
+namespace Simulation {
+
+   class Schedulable {
+
+      private:
+
+      public:
+
+   };
+
+   class Processor : public Schedulable {
+
+      private:
+
+      public:
+
+   };
+
+}
+
+#endif
