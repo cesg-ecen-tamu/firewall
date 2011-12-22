8b882a9a890fb9c005c2a5ff23bf7cfef489c7a4
Jasson Casey <jasson.casey@gmail.com>
2011-12-22 11:37:30 -0600

    More framework stuff.

--- Simulation.cc
+++ Simulation.cc
@@ -7,10 +7,24 @@ std::ostream& operator<<( std::ostream& out, const Schedulable& s ) {
    return out;
 }
 
+std::ostream& operator<<( std::ostream& out, const Event& e ) {
+   e.Print( out );
+   return out;
+}
+
 std::ostream& operator<<( std::ostream& out, const Processor& p ) {
    p.Print( out );
    return out;
 }
 
+std::ostream& operator<<( std::ostream& out, const Source& s ) {
+   s.Print( out );
+   return out;
+}
+
+std::ostream& operator<<( std::ostream& out, const Sink& s ) {
+   s.Print( out );
+   return out;
+}
 
 }
--- Simulation.h
+++ Simulation.h
@@ -34,19 +34,7 @@ namespace Simulation {
 
    };
    std::ostream& operator<<( std::ostream& out, const Schedulable& s ) ;
-
-   class Processor : public Schedulable {
-
-      private:
-
-      public:
-
-         virtual void Print( std::ostream& out ) const {
-         }
-
-   };
-   std::ostream& operator<<( std::ostream& out, const Processor& p ) ;
-
+   
    class Queue {
 
       private:
@@ -55,6 +43,57 @@ namespace Simulation {
       public:
 
    };
+
+   class Event : public Schedulable {
+
+      private:
+
+      public:
+
+         virtual void Print( std::ostream& out ) const {
+         }
+   };
+   std::ostream& operator<<( std::ostream& out, const Event& e ) ;
+
+   class Processor : public Schedulable {
+
+      private:
+
+      public:
+
+         virtual void Process( const Event& e ) {
+         }
+
+         virtual void Print( std::ostream& out ) const {
+         }
+
+   };
+   std::ostream& operator<<( std::ostream& out, const Processor& p ) ;
+
+   // Generic data scource that can create events to schedule as well as be a 
+   // schedulable object for later event creation
+   class Source : public Processor {
+
+      private:
+
+      public:
+
+         void Print( std::ostream& out ) const {
+         }
+   };
+   std::ostream& operator<<( std::ostream& out, const Source& s ) ;
+
+   // Generic data sink that can provide a termination point for events
+   // as well as be a schedulable object for later actions
+   class Sink : public Processor {
+
+      private:
+
+      public:
+         void Print( std::ostream& out ) const {
+         }
+   };
+   std::ostream& operator<<( std::ostream& out, const Sink& s ) ;
 
 }
 
